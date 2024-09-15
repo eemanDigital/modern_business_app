9bfca39e7bbc7eb6f19945f3c94bce04394f3fe6
eemanDigital <eemandigitalconcept@gamil.com>
2024-09-15 18:00:03 +0100

    fixing cors bug

--- backend/utils/secreteToken.js
+++ backend/utils/secreteToken.js
@@ -13,7 +13,7 @@ export const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    sameSite: 'none',
     secure: process.env.NODE_ENV === 'production',
   };
 
