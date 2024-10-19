91bc9792961253be1af768c17231ab2f0392f03c
eemanDigital <eemandigitalconcept@gamil.com>
2024-10-19 18:09:50 +0100

    fixed global error handler export error

--- backend/controllers/errorController.js
+++ backend/controllers/errorController.js
@@ -1,76 +1,3 @@
-// import AppError from '../utils/appError.js';
-
-// // Handle MongoDB cast errors (e.g., invalid ObjectId)
-// const handleCastErrorDB = (err) => {
-//   const message = `Invalid ${err.path}: ${err.value}.`;
-//   return new AppError(message, 400);
-// };
-
-// // Handle MongoDB duplicate field errors
-// const handleDuplicateFieldsDB = (err) => {
-//   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-//   const message = `Duplicate field value: ${value}. Please use another value!`;
-//   return new AppError(message, 400);
-// };
-
-// // Handle MongoDB validation errors
-// const handleValidationErrorDB = (err) => {
-//   const errors = Object.values(err.errors).map((el) => el.message);
-//   const message = `Invalid input data. ${errors.join('. ')}`;
-//   return new AppError(message, 400);
-// };
-
-// // Send error response in development
-// const sendErrorDev = (err, res) => {
-//   res.status(err.statusCode).json({
-//     status: err.status,
-//     error: err,
-//     message: err.message,
-//     stack: err.stack,
-//   });
-// };
-
-// // Send error response in production
-// const sendErrorProd = (err, res) => {
-//   // Operational, trusted error: send message to client
-//   if (err.isOperational) {
-//     res.status(err.statusCode).json({
-//       status: err.status,
-//       message: err.message,
-//     });
-
-//   // Programming or other unknown error: don't leak error details
-//   } else {
-//     console.error('ERROR 💥', err);
-
-//     res.status(500).json({
-//       status: 'error',
-//       message: 'Something went very wrong!',
-//     });
-//   }
-// };
-
-// // Global error handling middleware
-// const globalErrorHandler = (err, req, res, next) => {
-//   err.statusCode = err.statusCode || 500;
-//   err.status = err.status || 'error';
-
-//   if (process.env.NODE_ENV === 'development') {
-//     sendErrorDev(err, res);
-//   } else if (process.env.NODE_ENV === 'production') {
-//     let error = { ...err };
-//     error.message = err.message;
-
-//     if (error.name === 'CastError') error = handleCastErrorDB(error);
-//     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-//     if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
-
-//     sendErrorProd(error, res);
-//   }
-// };
-
-// export default globalErrorHandler;
-
 import AppError from '../utils/appError.js';
 
 // Handle MongoDB cast errors (e.g., invalid ObjectId)
@@ -124,7 +51,7 @@ const sendErrorProd = (err, res) => {
 };
 
 // Global error handling middleware
-const globalErrorHandler = (err, req, res, next) => {
+export const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
@@ -143,4 +70,4 @@ const globalErrorHandler = (err, req, res, next) => {
   }
 };
 
-export default globalErrorHandler;
+// export default globalErrorHandler;
